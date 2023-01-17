4b481852cf484766fd8a47c0daf9cbf26de56a4f
TRybina132 <tryb2516@gmail.com>
2023-01-17 18:08:34 +0200

    add extension for silo

---
+++ Orleans.PushNotifications/Orleans.PushNotifications.Server/Extensions/OrleansPushNotificationsExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Orleans.PushNotifications.Apple.Models;
+using Orleans.PushNotifications.Google.Models;
+using Orleans.PushNotifications.Extensions;
+
+
+namespace Orleans.PushNotifications.Server.Extensions
+{
+    public static class OrleansPushNotificationsExtensions
+    {
+        public static void AddIosPushNotifications(this IServiceCollection serviceCollection, AppleConfiguration config)
+        {
+            serviceCollection.AddApplePushNotifications(config);
+        }
+
+        public static void AddAndroidPushNotifications(this IServiceCollection serviceCollection,
+            GoogleConfiguration config)
+        {
+            serviceCollection.AddGooglePushNotifications(config);
+        }
+    }
+}
--- Orleans.PushNotifications/Orleans.PushNotifications.Server/Grains/PushNotificationGrain.cs
+++ Orleans.PushNotifications/Orleans.PushNotifications.Server/Grains/PushNotificationGrain.cs
@@ -1,10 +1,25 @@
-﻿using Orleans.Concurrency;
+﻿using ManagedCode.Communication;
+using Orleans.Concurrency;
+using Orleans.PushNotifications.Core.Interfaces;
+using Orleans.PushNotifications.Core.Models;
+using Orleans.PushNotifications.Server.Grains.Interfaces;
 
 namespace Orleans.PushNotifications.Server.Grains
 {
     [StatelessWorker]
-    public class PushNotificationGrain : Grain
+    public class PushNotificationGrain : Grain, IPushNotificationGrain
     {
-        
+        private readonly IPushNotificationsManager _pushNotificationsManager;
+
+        public PushNotificationGrain(IPushNotificationsManager pushNotificationsManager)
+        {
+            _pushNotificationsManager = pushNotificationsManager;
+        }
+
+        public async Task<Result<DeviceRegistration>> SendPushNotification(DeviceRegistration deviceRegistration, PushNotification pushNotification)
+        {
+            var result = await _pushNotificationsManager.SendPushAsync(deviceRegistration, pushNotification);
+            return result;
+        }
     }
 }
