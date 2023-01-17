1c5e8ef658b473fb1902dcd978aadfc2b2bff19b
Tanya <tryb2516@gmail.com>
2023-01-17 14:18:13 +0200

    add package and interfaces

---
+++ Orleans.PushNotifications/Orleans.PushNotifications/Core/Models/DeviceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Orleans.PushNotifications.Core.Models.Enums;
+
+namespace Orleans.PushNotifications.Core.Models
+{
+    public struct DeviceRegistration
+    {
+        public DeviceRegistration(string deviceToken, PushPlatforms platform)
+        {
+            DeviceToken = deviceToken;
+            Platform = platform;
+        }
+        public string DeviceToken { get; set; }
+        public PushPlatforms Platform { get; set; }
+    }
+}
---
+++ Orleans.PushNotifications/Orleans.PushNotifications/Interfaces/IBasePushSender.cs
@@ -0,0 +1,12 @@
+﻿using ManagedCode.Communication;
+using Orleans.PushNotifications.Core.Models;
+
+namespace Orleans.PushNotifications.Interfaces
+{
+    public interface IBasePushSender
+    {
+        Task<Result<DeviceRegistration>> SendPushAsync(DeviceRegistration registration,
+            PushNotification notification,
+            CancellationToken cancellationToken = default);
+    }
+}
