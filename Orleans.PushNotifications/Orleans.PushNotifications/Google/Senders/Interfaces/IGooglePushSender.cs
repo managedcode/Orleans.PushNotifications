eea2c17ed9e79dcd2f5052056bbac67f7342c354
Tanya <tryb2516@gmail.com>
2023-01-17 15:06:28 +0200

    add push sender for android

---
+++ Orleans.PushNotifications/Orleans.PushNotifications/Google/Senders/Interfaces/IGooglePushSender.cs
@@ -0,0 +1,8 @@
+﻿using Orleans.PushNotifications.Core.Interfaces;
+using Orleans.PushNotifications.Google.Models;
+
+namespace Orleans.PushNotifications.Google.Senders.Interfaces;
+
+public interface IGooglePushSender : IPushSender<GoogleNotification, FcmResponse>
+{
+}
