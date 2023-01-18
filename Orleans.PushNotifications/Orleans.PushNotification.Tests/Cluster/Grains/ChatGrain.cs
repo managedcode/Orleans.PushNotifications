43d797dd9deb5ed4c87220f596daa9784d706372
Tanya <tryb2516@gmail.com>
2023-01-18 02:46:51 +0200

    add silo configuration

---
+++ Orleans.PushNotifications/Orleans.PushNotification.Tests/Cluster/Grains/ChatGrain.cs
@@ -0,0 +1,12 @@
+﻿using Orleans.PushNotification.Tests.Cluster.Grains.Interfaces;
+
+namespace Orleans.PushNotification.Tests.Cluster.Grains
+{
+    public class ChatGrain : Grain, IChatGrain
+    {
+        public Task SendTestMessage()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Orleans.PushNotifications/Orleans.PushNotification.Tests/Cluster/Grains/Interfaces/IChatGrain.cs
@@ -0,0 +1,7 @@
+﻿namespace Orleans.PushNotification.Tests.Cluster.Grains.Interfaces
+{
+    public interface IChatGrain : IGrainWithStringKey
+    {
+        public Task SendTestMessage();
+    }
+}
