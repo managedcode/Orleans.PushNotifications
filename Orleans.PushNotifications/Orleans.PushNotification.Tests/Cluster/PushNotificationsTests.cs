da832647424fc458241e34c6deddb48fd7628657
Tanya <tryb2516@gmail.com>
2023-01-18 08:45:31 +0200

    add tests

---
+++ Orleans.PushNotifications/Orleans.PushNotification.Tests/Cluster/PushNotificationsTests.cs
@@ -0,0 +1,24 @@
+﻿using Orleans.PushNotification.Tests.Cluster.Grains.Interfaces;
+using Xunit;
+
+namespace Orleans.PushNotification.Tests.Cluster
+{
+    [Collection(nameof(TestClusterApplication))]
+    public class PushNotificationsTests
+    {
+        private readonly TestClusterApplication _testApp;
+
+        public PushNotificationsTests(TestClusterApplication testApp)
+        {
+            _testApp = testApp;
+        }
+
+        [Fact]
+        public async Task MakeCallToGrain_WhenGrainExists_ReturnOk()
+        {
+            var chatId = Guid.NewGuid().ToString();
+            var chatGrain = _testApp.Cluster.Client.GetGrain<IChatGrain>(chatId);
+            await chatGrain.SendTestMessage();
+        }
+    }
+}
