3ab36842ae5425d448a626cecf6e49704b976c36
Tanya <tryb2516@gmail.com>
2023-01-18 02:58:49 +0200

    add test application

---
+++ Orleans.PushNotifications/Orleans.PushNotification.Tests/Cluster/Configurations/TestClientConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Orleans.TestingHost;
+
+namespace Orleans.PushNotification.Tests.Cluster.Configurations
+{
+    public class TestClientConfiguration : IClientBuilderConfigurator
+    {
+        public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+        {
+
+        }
+    }
+}
---
+++ Orleans.PushNotifications/Orleans.PushNotification.Tests/Cluster/TestClusterApplication.cs
@@ -0,0 +1,31 @@
+﻿using Orleans.PushNotification.Tests.Cluster.Configurations;
+using Orleans.TestingHost;
+using Xunit;
+
+namespace Orleans.PushNotification.Tests.Cluster
+{
+    [CollectionDefinition(nameof(TestClusterApplication))]
+    public class TestClusterApplication : ICollectionFixture<TestClusterApplication>, IDisposable, IAsyncDisposable
+    {
+        public TestCluster Cluster { get; }
+
+        public TestClusterApplication()
+        {
+            var builder = new TestClusterBuilder();
+            builder.AddSiloBuilderConfigurator<TestSiloConfiguration>();
+            builder.AddClientBuilderConfigurator<TestClientConfiguration>();
+            Cluster = builder.Build();
+            Cluster.Deploy();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Cluster.DisposeAsync();
+        }
+
+        public void Dispose()
+        {
+            Cluster.Dispose();
+        }
+    }
+}
