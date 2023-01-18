e25d1adcd0b6f6385af57b66693e7055b7f12d5a
Tanya <tryb2516@gmail.com>
2023-01-18 14:27:03 +0200

    remove old project

--- Orleans.PushNotifications/Orleans.PushNotification.Tests/Constants/TokensForTesting.cs
+++
@@ -1,7 +0,0 @@
-namespace Orleans.PushNotification.Tests.Constants;
-
-public class TokensForTesting
-{
-    public const string DevicePushTokens =
-        "cJuPl6FaRx-ooj1VPI-i8W:APA91bGi1peNVS0_dnjfaLADDE34idrtvfUwUrPLZrqyBufOz2Xdi_khWorF_q6CZ0XxHvvUhZliknEKoI1iBRPJzplRGcK1L8_EkcZWo6fUpOvsupMQSWCak-PXkd9NWcPIfCHtqSdT";
-}
